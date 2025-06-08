e840e31ff5bd670639556fa575ce5886bb3814fc
SuprioBM <mugdharj632@gmail.com>
2025-06-09 00:52:50 +0600

    faka_page_error

---
+++ src/app/(root)/resume/page.tsx
@@ -0,0 +1,13 @@
+// src/app/(root)/resume/page.tsx
+import React from "react";
+
+const ResumePage = () => {
+  return (
+    <div>
+      <h1>My Resume</h1>
+      {/* Resume content here */}
+    </div>
+  );
+};
+
+export default ResumePage;
